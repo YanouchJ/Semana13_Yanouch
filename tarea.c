f63b8448c9073673300476eb304722665f59c008
YanouchJ <jeimy.yanouch@udla.edu.ec>
2024-01-17 20:10:37 -0500

    Configuración de GitHub e inicio del programa

---
+++ tarea.c
@@ -0,0 +1,18 @@
+/*1. Suba el enlace al repositorio del GitHub, con el código solicitado.
+2. Cada estudiante debe trabajar en el ejemplo de estructura de la clase anterior (alumno) y debe crear un programa
+que permita al usuario (menú) el ingreso de datos de n alumnos y además la presentación en pantalla de los alumnos ingresados.
+Además, debe utilizar las funciones:
+gets();
+puts();
+fflush();
+
+NOTA: no quemar los datos en el código*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num;
+    printf("Alumnos a registrar: ");
+    scanf("%d", &num);
+    
+    return 0;
+}
